aac03b510228fa89a3d32d2862cfc5b5f21a3c25
ibzaa1 <ibrahim.thaanbir@gmail.com>
2021-07-22 12:50:40 +0100

    :tada inital commit:

---
+++ components/ApartmentList.js
@@ -0,0 +1,11 @@
+import { Apartments } from '../data/apartments';
+
+const ApartmentList = () => {
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  );
+};
+
+export default ApartmentList;
---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className='text-center bg-gray-300 p-4 text-lg underline'>
+      <a className='mx-80 text-3xl'>Apartment Tracker</a>
+    </nav>
+  );
+};
+
+export default Navbar;
